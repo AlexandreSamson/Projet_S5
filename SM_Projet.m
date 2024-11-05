d4d165bfcacb3187242f43babf439c3152ccd6a7
AlexandreSamson <sama1402@USherbrooke.ca>
2024-11-05 09:38:17 -0500

    Update SM_Projet.m

--- SM_Projet.m
+++ SM_Projet.m
@@ -87,3 +87,11 @@ Gsm_SM_7 = tf(Gsm_num_SM_7, Gsm_den_SM_7);
 
 p_Gsm_SM_7 = roots(Gsm_den_SM_7); %poles de la FTBO
 
+
+%%
+figure
+plot(tsimu(1:end-1), omega_c)
+hold on;
+plot(tsimu, servo)
+title('Graph des valeurs expérimentales')
+xlabel('Temps (secondes)')
