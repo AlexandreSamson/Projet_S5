89eea8f3b8c513d2a338aa50fd264cc991fa4548
Alexplusplus5 <162742610+Alexplusplus5@users.noreply.github.com>
2024-11-06 17:36:11 -0500

    Big dawg changes
    En gros, Rm et Beq fonctionnent et SC est pas mal completer

--- Identification paramètre/SC_projet.m
+++ Identification paramètre/SC_projet.m
@@ -1,20 +1,37 @@
 %% SC1 
-clear all; clc;close all;load("data_1v_4-09_100hz.mat", "Vm", "servo", "omega_c", "tsimu");
+clear all; clc;close all;
+load("data_1v_4-09_100hz.mat", "Vm", "servo", "omega_c", "tsimu");
+%Variables
+ms  = 0.064;
+Js  = 4.129e-6;
+rs  = 0.0127;
+g   = 9.81;
+km  = 0.0076776;
+kt  = 0.0076830;
+nm  = 0.69;
+Jm  = 3.9001e-7;
+Jeq = 0.0017728; % Jeq = Jm + N^2*Jc
+ng  = 0.9;
+Kg  = 70;
+rarm= 0.0254;
+L   = 0.4254;
+N = ng*Kg;
 
 % Lissage par projection orthogonal 
 A_moy = mean(deg2rad(omega_c(114:end))); % A partie regime permanent
-y_ = abs(log(A_moy - deg2rad(omega_c(101:114)))./deg2rad(omega_c(101:114))); % 
+y_ = abs(log(A_moy - deg2rad(omega_c(101:108)))./deg2rad(omega_c(101:108))); % y 
 
 % Matrice coefficient C pour projection orthogonal
 i = 1;  
-for c = 101:114
+for c = 101:108
     A_coef(i, :) = [1, tsimu(c)]; 
     i = i + 1;
 end
 A_lis = pinv(A_coef)*y_;
-lambda = A_lis(2); % Alpha = - Lambda
-t0 = A_lis (1)/lambda; % Beta = lambda * t0
-omega_c_lisse = A_moy ./ (1 + exp(-lambda*(tsimu-t0))); % repartir de la fonction sigmoide
+lambda = -A_lis(2); % Alpha = - Lambda     --> Lambda = - Alpha
+t0 = A_lis(1) ./ lambda; % Beta = lambda * t0
+
+omega_c_lisse = A_moy ./ (1 + exp(lambda*(tsimu-t0))); % repartir de la fonction sigmoide
 
 % X_entree1 (X1) = u(t)
 X1 = Vm;
@@ -36,70 +53,39 @@ R = X' * X;
 P = X' * Y;
 
 Rinv = inv(R);
-A = Rinv*P;
+A_moindres_carres = Rinv*P;
 
-K = A(1);
-tau = -A(2); % Verifier le signe de Tau --> Équivalent a  :1 - alpha_sortie1 et comme A(2) négatif alors tau positif
-% Parti (d) 
-Jeq = 0.0017728; % unité : kg m^2 
+K = A_moindres_carres(1);
+tau = -A_moindres_carres(2); % Verifier le signe de Tau --> Équivalent a  :1 - alpha_sortie1 et comme A(2) négatif alors tau positif 
 % Calculer Rm et Beq à partir de la fonction de transfert standard
-
-
-
-
-
+Beq = (1./tau)*(Jeq*((N*nm*kt)./K-nm*kt*km))./((N*nm*kt)./K);
+Rm = (1./Beq)*(N*kt*nm./K-nm*kt*km);
 
 %% SC-2 R^2 et erreur RMS
-% Fonction de transfert à partir de valeur de lissage par moindre carrés 
-% Point (c) de SC-1. Valeur dans la section plus haut (tau et K);
-num = K;
-den = [-tau 1]; % tau ici est négatif voir Revue 2 section 3.1.4
-FT = tf(num, den); % des moindres carrés 
-[y_des, x_des] = lsim(FT, Vm,tsimu);
-
-y_mes = servo; % yn : c'est theta_c. C'est la valeur mesurée (_mes)
-
-E_squared = sum((y_des - y_mes).^2); 
+% Omega_c transformer en radian
+E_squared = sum((omega_c_lisse(1:end-1) - deg2rad(omega_c)).^2); 
 N = length(tsimu);
 RMSE = sqrt(1/N*E_squared);
 
-y_moy = 1/N*sum(y_mes);
-R_squared = sum((y_des - y_moy).^2)/(sum((y_mes - y_moy).^2));
+y_moy = 1/N*sum(deg2rad(omega_c));
+R_squared = sum((omega_c_lisse(1:end-1) - y_moy).^2)/(sum((deg2rad(omega_c) - y_moy).^2));
+
+%% SC-3 Reponse de la fct transfert Gcm(s) à l'échelon Vm et donnees experimentales 
+
+num = [(Kg*ng*nm*kt)./(Rm*Beq + nm*kt*km)];
+den = [(Rm*Jeq)./(Rm*Beq+nm*kt*km) 1];
+FT = tf(num, den); % des moindres carrés 
+[y_Gcm, x_Gcm] = lsim(FT, Vm,tsimu);
 
 figure
-plot(tsimu, y_des)
-hold on;
-plot(tsimu, servo)
-title("Moindre carrés vs Mesuré")
-legend('Moindres carrés', 'Données expérimentales')
-xlabel('Temps (secondes)')
-ylabel('Angle (theta_c)')
-
-%% SC-3 Reponse de la fct transfert Gcm(s) à l'échelon Vm
-
-Gcm_num = [K]; % Le signe doit il être négatif ?
-Gcm_den = [Rm*Jeq (Rm*Beq + nm*kt*km) 0];
-Gcm = tf(Gcm_num, Gcm_den);
-
-% Reponses a echelon 
-[yGcm, xGcm] = lsim(Gcm, Vm, tsimu);
-
-% Graphique des reponses a Vm 
-figure
-plot(xGcm, abs(yGcm)) % Ici malgré le fait que K soit négatif, comme on représente un angle -> Pertinent de l'avoir en Absolue (?)
+plot(x_Gcm, y_Gcm);
 hold on; 
-plot(tsimu, servo)
-title("Comparaison des systèmes")
-legend('Gcm', 'Données expérimentales')
+plot(tsimu(1:end-1), deg2rad(omega_c));
+title("Reponses à Vm de la FCT G_c_m(s) comparés aux données expérimentales")
 xlabel('Temps (secondes)')
-ylabel('Angle (theta_c)')
+ylabel('Vitesse (rad/s)')
+legend('FT G_C_M(s)', 'Données expérimentales')
 
-
-%% SI-1 
-figure
-rlocus(Gcm) % Allure non stable 
-figure
-rlocus(FT)
 
 
 
